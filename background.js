3a3be01c88229e9f881c7dd021f4e1efec8d3727
Matt Baer <matt@baer.works>
2017-08-30 22:45:22 -0400

    Add option to disable dynamic/lazy image loading
    This ensures all images in a post load immediately, instead of waiting
    for the user to scroll down the page.
    
    Closes #5

--- background.js
+++ background.js
@@ -20,6 +20,18 @@ var hideDickbar = function() {
 	document.querySelector('footer > .container:first-child').style.display = 'none';
 };
 
+var disableLazyLoading = function() {
+	var hiddenMedia = document.querySelectorAll('.js-progressiveMedia-inner');
+	if (hiddenMedia == null) {
+		return;
+	}
+	for (var i=0; i<hiddenMedia.length; i++) {
+		var template = document.createElement('template');
+		template.innerHTML = hiddenMedia[i].textContent;
+		hiddenMedia[i].parentNode.appendChild(template.content.firstChild);
+	}
+};
+
 // Only run this on Medium sites. 
 // Ensure that by checking for <meta property="al:ios:app_name" content="Medium"> in the document <head />
 var metaCheck = document.head.querySelector('meta[property="al:ios:app_name"]');
@@ -29,6 +41,9 @@ if (metaCheck != null && metaCheck.content == "Medium") {
 	chrome.storage.sync.get(null, function(items) {
 		if (items.hideDickbar) {
 			hideDickbar();
+		}
+		if (items.disableLazyImages) {
+			disableLazyLoading();
 		}
 	});
 }
