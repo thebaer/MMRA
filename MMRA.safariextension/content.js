edc2a25bb31b023c494e088aff706c36f4ed20c7
Dang Huynh <huynhminhdang@gmail.com>
2018-09-18 15:49:35 +0800

    Fix 59f5e12

--- MMRA.safariextension/content.js
+++ MMRA.safariextension/content.js
@@ -87,9 +87,9 @@ var config = {attributes: true};
 
 // This extension runs on all domains so it can Make Medium Readable Again even for publications on custom domains.
 // Here we make sure the code only runs on Medium sites.
+if (document.querySelector('head[prefix*="medium-com:"]')) {
 	makeReadable();
 	shrinkHeaderImages();
-if (document.querySelector('head[prefix*="medium-com:"]')) {
 
 	chrome.storage.sync.get(null, function(items) {
 		if (items.hideDickbar) {
